708341539b5c11185522c2eba3fcbbe7fd491aef
joon jang <joonjang95@gmail.com>
2019-01-31 00:37:38 -0500

    Remove comments in server

--- server/index.js
+++ server/index.js
@@ -6,14 +6,11 @@ const app = express();
 
 app.use(morgan('dev'));
 
-// PART 1
-
 // ****important for front-end routing****
 app.use(express.static(path.join(__dirname, '../public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-// *****************************************
 
 const port = process.env.PORT || 9000;
 
